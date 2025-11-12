a2c86ffaa38082d570cdf2a7311cb0ccbcccbe1d
steveetm <steve@powerednow.com>
2025-11-12 11:11:35 +0100

    Add methods to check and activate Tap to Pay
    Offer a dialog to select tap to pay or card reader.
    Call tap to pay activation if not yet activated.

--- src/ios/CDVSumUp.m
+++ src/ios/CDVSumUp.m
@@ -143,11 +143,79 @@
     NSString* currency = [command.arguments objectAtIndex:1];
     NSString* title = [command.arguments objectAtIndex:2];
 
+    [SMPSumUpSDK checkTapToPayAvailability:^(BOOL isAvailable, BOOL isActivated, NSError * _Nullable error) {
+        if (error == nil && isAvailable) {
+            [self showPaymentMethodSelectionForTotal:total currency:currency title:title isActivated:isActivated command:command];
+        } else {
+            [self processPaymentWithTotal:total currency:currency title:title paymentOption:SMPPaymentOptionCardReader command:command];
+        }
+    }];
+}
+
+-(void) showPaymentMethodSelectionForTotal:(NSDecimal)total currency:(NSString*)currency title:(NSString*)title isActivated:(BOOL)isActivated command:(CDVInvokedUrlCommand*)command {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Select Payment Method"
+                                                                   message:@"Choose how to process this payment"
+                                                            preferredStyle:UIAlertControllerStyleActionSheet];
+
+    UIAlertAction *cardReaderAction = [UIAlertAction actionWithTitle:@"Card Reader"
+                                                               style:UIAlertActionStyleDefault
+                                                             handler:^(UIAlertAction * _Nonnull action) {
+        [self processPaymentWithTotal:total currency:currency title:title paymentOption:SMPPaymentOptionCardReader command:command];
+    }];
+
+    UIAlertAction *tapToPayAction = [UIAlertAction actionWithTitle:@"Tap to Pay"
+                                                             style:UIAlertActionStyleDefault
+                                                           handler:^(UIAlertAction * _Nonnull action) {
+        if (isActivated) {
+            [self processPaymentWithTotal:total currency:currency title:title paymentOption:SMPPaymentOptionTapToPay command:command];
+        } else {
+            [SMPSumUpSDK presentTapToPayActivationFromViewController:self.viewController
+                                                             animated:YES
+                                                      completionBlock:^(BOOL success, NSError * _Nullable activationError) {
+                if (success) {
+                    [self processPaymentWithTotal:total currency:currency title:title paymentOption:SMPPaymentOptionTapToPay command:command];
+                } else {
+                    NSDictionary *dict = @{
+                                           @"code" : @0,
+                                           @"message" : @"Tap to Pay activation cancelled"
+                                           };
+                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:dict];
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }
+            }];
+        }
+    }];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+        NSDictionary *dict = @{
+                               @"code" : @0,
+                               @"message" : @"Payment cancelled"
+                               };
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:dict];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+
+    [alert addAction:cardReaderAction];
+    [alert addAction:tapToPayAction];
+    [alert addAction:cancelAction];
+
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        alert.popoverPresentationController.sourceView = self.viewController.view;
+        alert.popoverPresentationController.sourceRect = CGRectMake(self.viewController.view.bounds.size.width / 2.0, self.viewController.view.bounds.size.height / 2.0, 1.0, 1.0);
+    }
+
+    [self.viewController presentViewController:alert animated:YES completion:nil];
+}
+
+-(void) processPaymentWithTotal:(NSDecimal)total currency:(NSString*)currency title:(NSString*)title paymentOption:(SMPPaymentOptions)paymentOption command:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = nil;
     SMPCheckoutRequest *request = [SMPCheckoutRequest requestWithTotal:[NSDecimalNumber decimalNumberWithDecimal:total] title:title
         currencyCode:currency];
 
     [request setSkipScreenOptions:SMPSkipScreenOptionSuccess];
+    [request setPaymentOptions:paymentOption];
 
     [SMPSumUpSDK checkoutWithRequest:request fromViewController:self.viewController completion:^(SMPCheckoutResult *result, NSError *error) {
         CDVPluginResult* pluginResult = nil;
@@ -158,16 +226,16 @@
 //          }
           NSDictionary *card = result.additionalInfo[@"card"];
           NSMutableDictionary *dict = [NSMutableDictionary new];
-  
+
           NSArray *keysToCheck = @[@"transaction_code", @"merchant_code", @"amount", @"tip_amount", @"vat_amount",
                             @"currency", @"status", @"payment_type", @"entry_mode"];
-  
+
           for (NSString *key in keysToCheck) {
               if (result.additionalInfo[key]) {
                   dict[key] = result.additionalInfo[key];
               }
           }
-  
+
           if (card[@"type"]) {
               dict[@"card_type"] = card[@"type"];
           }
@@ -244,6 +312,5 @@
   CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
 
 @end
